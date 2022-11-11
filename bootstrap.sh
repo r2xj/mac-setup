358304388110c13afee5c17bb7de0b8163f21568
r2xj <roryjensen@gmail.com>
2022-11-11 14:05:03 -0700

    added casks and mas

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,9 +37,13 @@ then
   echo "oh-my-zsh already installed."
 else
   echo "Not found, installing Oh My ZSH..."
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  echo "Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  echo "Installing zsh-syntax-highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
-
+ 
 
 echo "Checking for PowerLevel10K zsh theme..."
 if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]
@@ -55,11 +59,6 @@ else
   cd -; }
   echo "Installing Powerlevel10k zsh theme..."
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  echo "Installing zsh-syntax-highlighting..."
-  brew install zsh-syntax-highlighting
-  echo "Installing zsh-autosuggestions..."
-  brew install zsh-autosuggestions
-
 fi
 
 
